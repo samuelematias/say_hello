6e853470990861160e6c92524e5a43146e04289e
Samuel Matias <hi@samuelematias.com>
2022-03-29 20:33:50 -0300

    feat: update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:widgetbook_challenge/app.dart';
+import 'package:widgetbook_challenge/app/app.dart';
+import 'package:widgetbook_challenge/app/app_bloc_observer.dart';
 
 void main() {
-  runApp(const App());
+  WidgetsFlutterBinding.ensureInitialized();
+  BlocOverrides.runZoned(
+    () => runApp(const App()),
+    blocObserver: AppBlocObserver(),
+  );
 }

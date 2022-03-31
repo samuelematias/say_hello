e5db39cd17ab22d2131f66ba16da75380142c69b
Samuel Matias <hi@samuelematias.com>
2022-03-31 07:18:52 -0300

    fix: relative import

--- lib/main_desktop.dart
+++ lib/main_desktop.dart
@@ -1,4 +1,4 @@
-import 'main.dart' as original_main;
+import 'package:widgetbook_challenge/main.dart' as original_main;
 
 // This file is the default main entry-point for go-flutter application.
 void main() {

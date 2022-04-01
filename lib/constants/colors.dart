8c0297d3c6e445fdce5867150edb756ca3a2bf6b
Samuel Matias <hi@samuelematias.com>
2022-04-01 19:53:31 -0300

    refactor: concentrating constants in a single file

--- lib/constants/colors.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/painting.dart';
-
-/// The main color of the app
-const brandColor = Color(0xFF772CE8);
--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,1 +1,21 @@
-export 'colors.dart';
+import 'package:flutter/painting.dart';
+
+/// Color representing the brand in the app
+const brandColor = Color(0xFF772CE8);
+
+/// Color representing errors in the app
+const errorColor = Color(0xFFD21F3C);
+
+/// Default padding value used in app
+const defaultPaddingValue = 16.0;
+
+/// Default error message used in app
+const defaultErrorMessage = 'Something is wrong! Try again later.';
+
+/// Default timeout error message used in app
+const defaultTimeOutErrorMessage =
+    'Looks like the server is taking to long to respond, '
+    'please try again.';
+
+/// Invalid entered value message
+const invalidEnteredValueMessage = 'This field just accept letters [A - Z].';

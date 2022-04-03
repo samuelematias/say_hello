96f7f7261a8531678765bbd503429298b5c5dec4
Samuel Matias <hi@samuelematias.com>
2022-04-03 17:50:45 -0300

    chore: add new color/keys

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,10 +1,13 @@
-import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 
 /// Color that representing widgets with our brand in the app
 const brandColor = Color(0xFF772CE8);
 
 /// Color that representing widgets with errors in the app
 const errorColor = Color(0xFFD21F3C);
+
+/// Color that representing widgets with warning in the app
+const warningColor = Color(0xFFEED202);
 
 /// Color that representing widgets disabled in the app
 const disabledColor = Color(0xFFA9A9A9);
@@ -21,3 +24,22 @@ const defaultApiErrorMessage =
 
 /// Invalid entered value message
 const invalidEnteredValueMessage = 'This field just accept letters [A - Z].';
+
+/// LoadingIndicator widget - identification key
+const homePageLoadingIndicatorWidgetKey = Key('loading_indicator');
+
+/// Showing the Response value (Hello + typedValue) on success case
+/// - identification key
+const messageSuccessWidgetKey = Key('message_success');
+
+/// Showing default error message - identification key
+const defaultErrorWidgetKey = Key('default_error');
+
+/// Showing default API error message - identification key
+const defaultApiErrorWidgetKey = Key('default_api_error');
+
+/// Invalid entered value - identification key
+const invalidEnteredValueWidgetKey = Key('invalid_entered_value');
+
+/// "Say, Hello!" button identification - key
+const confirmButtonWidgetKey = Key('confirm_button');
